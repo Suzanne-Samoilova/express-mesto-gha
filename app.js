c5f42c527091b286f451b51048144d2f76d0eb0c
Suzanne Samoilova <suzelle@yandex.ru>
2022-04-25 00:13:05 +0300

    feat: package.json, editorconfig, ESLint, gitignore, nodemon

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.get('/', (req, res) => {
+  res.send('<button>88888888</button>')
+})
+
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`)
+})
+
+module.exports = app;
