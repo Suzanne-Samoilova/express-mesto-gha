cdb1015586a358c711f5f2ab2709a68a0763c903
Suzanne Samoilova <suzelle@yandex.ru>
2022-05-19 17:24:16 +0300

    fix: eslint errors

--- models/user.js
+++ models/user.js
@@ -58,7 +58,7 @@ const userSchema = new mongoose.Schema({
 
 // добавим метод findUserByCredentials схеме пользователя
 // у него будет два параметра — почта и пароль
-userSchema.statics.findUserByCredentials = function (email, password) {
+userSchema.statics.findUserByCredentials = function fn(email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
