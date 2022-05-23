10bcbdfbc667eefa3c73bb2e600f710e48b59bdf
Suzanne Samoilova <suzelle@yandex.ru>
2022-05-23 11:19:04 +0300

    fix: login in user-controllers (return this)

--- controllers/users.js
+++ controllers/users.js
@@ -137,7 +137,7 @@ module.exports.updateAvatar = (req, res, next) => {
 // -----------------------------------------------------------------------------
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-  return User.findUserByCredentials(email, password)
+  return this.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
