1285f50c8a56f93cd7353a19d45ab693120a5cd6
Suzanne Samoilova <suzelle@yandex.ru>
2022-06-21 10:04:09 +0300

    fix: eslint error

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const { login, createUser } = require('./controllers/users');
 const { validateUser, validateLogin } = require('./middlewares/validations');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-
 const { PORT = 3000 } = process.env;
 const app = express();
 
