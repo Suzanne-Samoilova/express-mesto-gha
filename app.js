45f124dcfebeba7282f3ed6a9c66e0108358f615
Suzanne Samoilova <suzelle@yandex.ru>
2022-06-21 11:13:00 +0300

    fix: cors options in app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.use(cors);
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
